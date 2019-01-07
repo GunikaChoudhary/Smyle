225acaddf85dfea8984154b5f53d4068ea84a85d
jahanvee-narang <30794272+jahanvee-narang@users.noreply.github.com>
2019-01-08 01:22:35 +0530

    Update JoinUS.java

--- app/src/main/java/com/example/jahanveenarang/smyle/JoinUS.java
+++ app/src/main/java/com/example/jahanveenarang/smyle/JoinUS.java
@@ -2,12 +2,22 @@ package com.example.jahanveenarang.smyle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class JoinUS extends AppCompatActivity {
 
+    android.support.v7.widget.Toolbar  toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_us);
+        
+        toolbar= findViewById(R.id.toolbar_joinUs);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Join Us");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        
+        
     }
 }
