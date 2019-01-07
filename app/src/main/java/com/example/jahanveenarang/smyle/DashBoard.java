40f6085dba21de6b489821c7e3cde1fc6b42c905
jahanvee-narang <30794272+jahanvee-narang@users.noreply.github.com>
2019-01-08 01:13:42 +0530

    Update DashBoard.java

--- app/src/main/java/com/example/jahanveenarang/smyle/DashBoard.java
+++ app/src/main/java/com/example/jahanveenarang/smyle/DashBoard.java
@@ -1,5 +1,6 @@
 package com.example.jahanveenarang.smyle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,16 +13,68 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DashBoard extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    ImageView about , donate ,refer ;
+    TextView educateGirls, bookTrips , joinUs, contribution  ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+       about = findViewById(R.id.about);
+       donate = findViewById(R.id.donate);
+       refer = findViewById(R.id.refer);
+
+       educateGirls = findViewById(R.id.educategirls);
+       bookTrips= findViewById(R.id.booktrips);
+       joinUs = findViewById(R.id.joinus);
+       contribution = findViewById(R.id.contributions);
+
+
+       about.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i = new Intent(DashBoard.this , AboutActivity.class);
+               startActivity(i);
+           }
+       });
+
+        educateGirls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DashBoard.this , GiveEducation100children.class);
+                startActivity(i);
+            }
+        });
+
+        bookTrips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        joinUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DashBoard.this , JoinUS.class);
+                startActivity(i);
+            }
+        });
+        contribution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DashBoard.this , Contributions.class);
+                startActivity(i);
+            }
+        });
+
 
 
 
@@ -73,19 +126,54 @@ public class DashBoard extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+        if (id == R.id.about) {
+
+            Intent i = new Intent(DashBoard.this , AboutActivity.class);
+            startActivity(i);
+        } else if (id == R.id.news) {
+
+        } else if (id == R.id.sponser) {
+            Intent i = new Intent(DashBoard.this , GiveEducation100children.class);
+            startActivity(i);
+
+        } else if (id == R.id.contributions) {
+
+            Intent i = new Intent(DashBoard.this , Contributions.class);
+            startActivity(i);
+
+
+        } else if (id == R.id.supporters) {
+            Intent i = new Intent(DashBoard.this , Supporters.class);
+            startActivity(i);
+
+        }
+        else if (id == R.id.share) {
+
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Smyle");
+            String shareMessage= "\nLet me recommend you this application\n\n";
+            shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+
+        }
+        else if (id == R.id.join) {
+            Intent i = new Intent(DashBoard.this , JoinUS.class);
+            startActivity(i);
+
+        }
+        else if (id == R.id.write) {
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/html");
+            intent.putExtra(Intent.EXTRA_EMAIL, "4smylefoundation@gmail.com");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Regarding ngo support");
+            intent.putExtra(Intent.EXTRA_TEXT, "Respected sir, ");
+
+            startActivity(Intent.createChooser(intent, "Send Email"));
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
